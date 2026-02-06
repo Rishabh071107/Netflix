07ec761ded5abf46ba598071ab931b1bc499576e
Rishabh071107 <rishabh71107@gmail.com>
2026-02-06 14:57:26 +0530

    file created

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,31 +4,57 @@ import facebook_icon from '../assets/facebook_icon.png'
 import twitter_icon from '../assets/twitter_icon.png'
 import instagram_icon from '../assets/instagram_icon.png'
 import youtube_icon from '../assets/youtube_icon.png'
+
 function Footer() {
   return (
-    <div className='footer'>
-      <div className='footer-icons'>
-        <img src={facebook_icon} alt='facebook'/>
-        <img src={twitter_icon} alt='twitter'/>
-        <img src={instagram_icon} alt='instagram'/>
-        <img src={youtube_icon} alt='youtube'/>
-      </div>
-      <ul>
-        <li>Audio description</li>
-        <li>Help Center</li>
-        <li>Gift Cards</li>
-        <li>Media Center</li>
-        <li>Investor Relations</li>
-        <li>Jobs</li>
-        <li>Terms of Use</li>
-        <li>Privacy</li>
-        <li>Cookie Preferences</li>
-        <li>Corporate Information</li>
-        <li>Contact Us</li>
+    <footer className='footer'>
 
-      </ul>
-      <p className='copyright-text'>© 2024 Netflix . All rights reserved.</p>
-    </div>
+      <div className='footer-container'>
+
+        <div className='footer-social'>
+          <img src={facebook_icon} alt='facebook'/>
+          <img src={twitter_icon} alt='twitter'/>
+          <img src={instagram_icon} alt='instagram'/>
+          <img src={youtube_icon} alt='youtube'/>
+        </div>
+
+        <div className='footer-links'>
+          <ul>
+            <li>Audio Description</li>
+            <li>Help Center</li>
+            <li>Gift Cards</li>
+            <li>Media Center</li>
+          </ul>
+
+          <ul>
+            <li>Investor Relations</li>
+            <li>Jobs</li>
+            <li>Terms of Use</li>
+            <li>Privacy</li>
+          </ul>
+
+          <ul>
+            <li>Cookie Preferences</li>
+            <li>Corporate Information</li>
+            <li>Contact Us</li>
+            <li>Legal Notices</li>
+          </ul>
+
+          <ul>
+            <li>Account</li>
+            <li>Ways to Watch</li>
+            <li>Only on Netflix</li>
+            <li>Speed Test</li>
+          </ul>
+        </div>
+
+        <div className='footer-bottom'>
+          <p>© 2026 Netflix Clone by Rishabh. All rights reserved.</p>
+        </div>
+
+      </div>
+
+    </footer>
   )
 }
 
